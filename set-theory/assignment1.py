484e2c57251855223d575c14edfa336ca9dff6f5
Mark Feltner <feltner.mj@gmail.com>
2012-01-25 19:39:07 -0600

    calculate all positive integers of a number and determine whether that number is perfect.

---
+++ set-theory/assignment1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def positive_divisors(n):
+    ''' returns a list of positive divisors of n
+    For example: 6 has four positive divisors [1,2,3,6]
+    '''
+    return [x for x in range(1, n+1) if n % x == 0]
+
+def perfect(n):
+    ''' returns True/False if a number is/isn't perfect.
+    An integer n is called perfect provided it equals the sum of all its
+    divisors that are both positive and less than n.
+    '''
+    return sum([x for x in positive_divisors(n) if x != n]) == n
+
